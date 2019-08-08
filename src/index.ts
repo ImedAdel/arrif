f824ca627528b2b990d831eda1857b3beadf84cf
Imed Adel <aadelimed@gmail.com>
2019-08-08 12:24:10 +0200

    define arrif and set up tests

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
-export const sum = (a: number, b: number) => {
-  if ('development' === process.env.NODE_ENV) {
-    console.log('boop');
-  }
-  return a + b;
+/**
+ * Alternative implementation of if-else statement
+ * @param t Returned if true
+ * @param f Returned if false
+ * @param c Condition
+ */
+export const arrif = (t: any, f: any, c: (number|boolean|string|null|undefined)): any => {
+  return [f, t][~~!!c];
 };
+
+export default arrif;
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,7 +1,27 @@
-import { sum } from '../src';
+import { arrif } from '../src';
 
-describe('blah', () => {
-  it('works', () => {
-    expect(sum(1, 1)).toEqual(2);
+describe('Conditional Array', () => {
+  it('returns true condition', () => {
+    expect(arrif('true', 'false', true)).toEqual('true');
+  });
+
+  it('returns false condition', () => {
+    expect(arrif('true', 'false', false)).toEqual('false');
+  });
+
+  it('works with empty strings', () => {
+    expect(arrif('true', 'false', '')).toEqual('false');
+  });
+
+  it('works with null', () => {
+    expect(arrif('true', 'false', null)).toEqual('false');
+  });
+
+  it('works with undefined', () => {
+    expect(arrif('true', 'false', undefined)).toEqual('false');
+  });
+
+  it('can be nested', () => {
+    expect(arrif(arrif('if', 'else if', true), 'else', true)).toEqual('if');
   });
 });
